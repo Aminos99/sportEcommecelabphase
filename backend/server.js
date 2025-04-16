8554c241726bfbd451265347e1ff500af56e8662
unknown <amineltaief06@gmail.com>
2025-04-16 23:00:43 +0100

    'version1'

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
+const path = require('path');
 const app = express();
 const UserRoutes = require('./routes/User.route');
 const OrderRoutes = require('./routes/Order.route')
 const ProductRoutes = require('./routes/Product.route')
 require('dotenv').config();
+
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -19,15 +22,19 @@ mongoose.connect(process.env.MONGO_URI, {
     console.error('Error connecting to MongoDB:', error);
     process.exit(1); 
   });
-
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 app.use('/api/user', UserRoutes);
 app.use('/api/orders', OrderRoutes);
 app.use('/api/products', ProductRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
